8d9f1091f4247a473909b4548709396eaf732847
clami66 <claudio.mirabello@scilifelab.se>
2022-06-07 17:11:14 +0200

    working algorithm on simple tests

--- src/lDDT.py
+++ src/lDDT.py
@@ -6,47 +6,34 @@ from Bio import PDB
 from Bio.PDB import PDBParser
 from Bio.SeqUtils import seq1
 from scipy.spatial.distance import cdist
-np.set_printoptions(threshold=maxsize)
+import matplotlib.pyplot as plt
+#np.set_printoptions(threshold=maxsize)
 
-def cache_distances(pdb):
+def cache_distances(pdb, atom_type="CA"):
     
     backbone_ids = ["N", "CA", "C", "O"]    
     model = pdb
     nres = len([_ for _ in model.get_residues() if PDB.is_aa(_)])
 
-    coords = np.zeros((2, nres, 3))
+    coords = np.zeros((nres, 3))
     sequence = ""
 
     for i, residue in enumerate([_ for _ in model.get_residues() if PDB.is_aa(_)]):
         centroid_counter = 1
         for atom in residue.get_atoms():
             if atom.get_id() == "CA":
-                coords[0, i,     :] = atom.get_coord()                
-                coords[1, i, :] = atom.get_coord()
+                coords[i,     :] = atom.get_coord()
                 sequence += seq1(residue.get_resname())
-            elif atom.get_id() not in backbone_ids:
-                coords[1, i, :] += atom.get_coord()
-                centroid_counter += 1
-        coords[1, i, :] /= centroid_counter
+            #elif atom.get_id() not in backbone_ids:
+            #    coords[1, i, :] += atom.get_coord()
+            #    centroid_counter += 1
+        #coords[1, i, :] /= centroid_counter
 
-    ca_distances = cdist(coords[0], coords[0])
-    ce_distances = cdist(coords[1], coords[1])
-    ca_distances[np.where(np.sum(coords, axis=1) == 0), :] = ca_distances[:, np.where(np.sum(coords, axis=1) == 0)] = 0
-    ce_distances[np.where(np.sum(coords, axis=1) == 0), :] = ce_distances[:, np.where(np.sum(coords, axis=1) == 0)] = 0
-    
-    distances = np.array([ca_distances, ce_distances])
+    distances = cdist(coords, coords)
+    distances[np.where(np.sum(coords, axis=1) == 0), :] = distances[:, np.where(np.sum(coords, axis=1) == 0)] = 0
+
     return sequence, distances
 
-def distance_difference(dist1, dist2, thresholds):
-    d = 0
-    distance_diff = np.abs(dist1 - dist2)
-    n_total = dist1.shape[-1] if dist1.shape[-1] > 0 else 1
-    #print(n_total, dist1.shape)
-    for threshold in thresholds:
-        n_conserved = np.count_nonzero(distance_diff < threshold)
-        d += n_conserved/n_total
-    
-    return d/len(thresholds)
 
 def lDDT(dist1, dist2, thresholds=[0.5, 1, 2, 4], r0=15.0):
 
@@ -58,130 +45,112 @@ def lDDT(dist1, dist2, thresholds=[0.5, 1, 2, 4], r0=15.0):
         
     return lddt
 
-def backtrack(picks, seq1, seq2, i, j):
-    aln = ""
+def traceback(trace, seq1, seq2, i, j):
+    aln1 = ""
+    aln2 = ""
+    i = trace.shape[0] - 1
+    j = trace.shape[1] - 1
+    while i >= 0 if j > i else j >= 0:
+        while j >= 0 if i > j else i >= 0:
+            if trace[i, j] == 0:
+                aln1 += seq1[i]
+                aln2 += seq2[j]
+                i -= 1
+                j -= 1
+            elif trace[i, j] == 1:
+                aln1 += "-" 
+                aln2 += seq2[j]
+                j -= 1
+            elif trace[i, j] == 2:
+                aln1 += seq1[i]
+                aln2 += "-" 
+                i -= 1
+        while i >= 0:
+            aln1 += seq1[i]
+            aln2 += "-"
+            i -= 1
+        while j >= 0:
+            aln2 += seq2[j]
+            aln1 += "-"
+            j -= 1
+    return aln1[::-1], aln2[::-1]
+
+def distance_difference(dist1, dist2, thresholds):
+    d = 0
+    n_dist1 = dist1.shape[-1]
+    n_dist2 = dist2.shape[-1]
+
+    n_dist = min(n_dist1, n_dist2)
+    distance_diff = np.abs(dist1[:,:n_dist] - dist2[:,:n_dist])
+    n_total = n_dist1 if n_dist1 > 0 else 1
+
+    for threshold in thresholds:
+        n_conserved = np.count_nonzero(distance_diff < threshold)
+        d += n_conserved/n_total
     
-    if i > 0 or j > 0:
-        if picks[i, j] == 0:
-            return backtrack(picks, seq1, seq2, i-1, j-1) + seq1[i] + seq2[j] + aln
-        elif picks[i, j] == 1:
-            return backtrack(picks, seq1, seq2, i, j-1)  + "-" + seq2[j] + aln
-        elif picks[i, j] == 2:
-            return backtrack(picks, seq1, seq2, i-1, j) + seq1[i] + "-" + aln
-    else:
-        if picks[i, j] == 0:
-            return aln + seq1[i] + seq2[j]
-        elif picks[i, j] == 1:
-            return aln + "-" + seq2[j]
-        elif picks[i, j] == 2:
-                return aln + seq1[i] + "-"
+    return d/len(thresholds)
 
 
 def score_match(dist1, dist2, i, j, selection, thresholds):
-    return sum([distance_difference(dist1[c, i, np.where(selection[c,i,:])], dist2[c, j, np.where(selection[c,i,:])], thresholds) for c in range(2)])/2
+    selection1 = np.where(selection[i, :])
+    selection2 = selection1[0] + j - i
+    selection2 = (selection2[selection2 < dist2.shape[-1]],)
+
+    return distance_difference(dist1[i, selection1], 
+                               dist2[j, selection2], thresholds)
 
 
-def needle2(dist1, dist2, seq1, seq2, thresholds=[0.5, 1, 2, 4], r0=15.0):
+def dynamic_programming(dist1, dist2, seq1, seq2, thresholds=[0.5, 1, 2, 4], r0=15.0):
     l1 = dist1.shape[-1]
     l2 = dist2.shape[-1]
-    table = np.zeros((l1+1, l2+1))
-    table[:,0] = table[0,:] = 0
-    picks = np.zeros((l1, l2))
+    local_lddt = np.zeros((l1, l2))
+    table = np.zeros((l1, l2))
+    trace = np.zeros((l1, l2))
     
     selection = (dist1 < r0) & (dist1 != 0)
     dist2[dist2 == 0] = max(thresholds) + 1
     
     # fill in table
-    for i in range(1, l1+1):
-        for j in range(1, l2+1):
-            delete = table[i-1, j]
-            insert = table[i, j-1]
+    for i in range(0, l1):
+        for j in range(0, l2):
+            delete = table[i-1, j] if i > 0 else 0
+            insert = table[i, j-1] if j > 0 else 0
 
-            match = table[i-1, j-1] #if (i > 0 and j > 0) else 0
-            match += score_match(dist1, dist2, i-1, j-1, selection, thresholds)
-            if match >= insert and match >= delete:
+            match = table[i-1, j-1] if i > 0 and j > 0 else 0
+            local_lddt[i,j] = score_match(dist1, dist2, i, j, selection, thresholds)
+            match += local_lddt[i,j]
+            
+            if match > insert and match > delete:
                 table[i,j] = match
-                picks[i-1,j-1] = 0
-            elif insert >= delete:
+                trace[i,j] = 0
+            elif insert > delete:
                 table[i,j] = insert
-                picks[i-1,j-1] = 1
+                trace[i,j] = 1
             else:
                 table[i,j] = delete
-                picks[i-1,j-1] = 2
-    print(picks[:5,:5])
+                trace[i,j] = 2
+    plt.imshow(local_lddt, cmap ='Greens')
+    plt.show()
+
     global_lddt = table[-1, -1]/min(l1, l2)
     
     # backtracking
-    alignments = backtrack(picks, seq1, seq2, picks.shape[0]-1, picks.shape[1]-1)
+    alignments = traceback(trace, seq1, seq2, trace.shape[0]-1, trace.shape[1]-1)
     
-    print(alignments[::2])
-    print(alignments[1::2])
+    print(alignments[0])
+    print(alignments[1])
     with open("foo", "w") as out:
         out.write("/ " + " ".join(seq2) + "\n")
-        for i, row in enumerate(table):
-            if i > 0:
-                out.write(seq1[i-1] + " ")
-            else:
-                pass
-            for j,col in enumerate(row):
-                
-                out.write(str(col) + " ")
-                
-            out.write("\n")
-    
-    return global_lddt
-
-    
-def needle(dist1, dist2, seq1, seq2, thresholds=[0.5, 1, 2, 4], r0=15.0):
-    l1 = dist1.shape[0]
-    l2 = dist2.shape[0]
-    table = np.zeros((l1, l2))
-    picks = np.zeros((l1, l2))
-    selection = (dist1 < r0) & (dist1 != 0)
-    dist2[dist2 == 0] = max(thresholds) + 1
-    
-    # fill in table
-    selected = np.where(selection[0,:l2])
-    init = distance_difference(dist1[1,selected], dist2[1,selected], thresholds)
-    picks[0, :] = 1
-    picks[:, 0] = 2
-    picks[0, 0] = 0
-    table[0, :] = table[:, 0] = init    
-    for i in range(1, l1):
-        for j in range(1, l2):
-            selected = np.where(selection[i,:l2-j])
-            delete = table[i-1, j]
-            insert = table[i, j-1]
-
-            match = table[i-1, j-1] + distance_difference(dist1[i, selected], dist2[j, selected], thresholds)
-
-            if match >= insert and match >= delete:
-                table[i,j] = match
-                picks[i,j] = 0
-            elif insert >= delete:
-                table[i,j] = insert
-                picks[i,j] = 1
-            else:
-                table[i,j] = delete
-                picks[i,j] = 2
-
-    global_lddt = table[-1, -1]/min(l1, l2)
-    
-    # backtracking
-    picks = picks[1::2,1::2]
-    alignments = backtrack(picks, seq1, seq2, picks.shape[0]-1, picks.shape[1]-1)
-    
-    print(alignments[::2])
-    print(alignments[1::2])
-    with open("foo", "w") as out:
-        out.write("/" + " ".join(seq2) + "\n")
-        for i,row in enumerate(picks):
+        for i, row in enumerate(trace):
             out.write(seq1[i] + " ")
             for j,col in enumerate(row):
-                out.write(str(int(col)) + " ")
+                
+                out.write(str((col)) + " ")
+                
             out.write("\n")
     
     return global_lddt
+
     
 def main():
     pdb1 = argv[1]
@@ -198,8 +167,7 @@ def main():
         
     decoy_seq, decoy_distances = cache_distances(decoy)        
     ref_seq, ref_distances = cache_distances(ref)
-    #lddt = lDDT(ref_distances, decoy_distances)
-    lddt = needle2(ref_distances, decoy_distances, ref_seq, decoy_seq)
+    lddt = dynamic_programming(ref_distances, decoy_distances, ref_seq, decoy_seq, thresholds=[4])
     print(lddt)
 
 if __name__ == "__main__":
